234ac22f3c9f0c315b46ea098478fae46aff136b
Peter <peter.zmilczak@gmail.com>
2023-10-09 20:52:36 +0200

    added project object init

--- valhalla/main.py
+++ valhalla/main.py
@@ -1,5 +1,6 @@
 from valhalla.ci_provider.gitlab.get_version import get_version
 from valhalla.common.get_config import get_config
+from valhalla.model.project import Project
 
 
 def start():
@@ -7,6 +8,7 @@ def start():
     print(f'Release the Valhalla!')  # Press Ctrl+F8 to toggle the breakpoint.
     version = get_version()
     config = get_config("./valhalla.yml")
+    project = Project(config.project_name, version)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
