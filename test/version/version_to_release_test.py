c57f891cbd6783f0d7e2cde626505bdd779ab41f
pz2 <piotr.zmilczak@softnet.com.pl>
2024-05-18 15:21:32 +0200

    Added release kinds to allow creating many valhalla.yml files to define different processes

--- test/version/version_to_release_test.py
+++ test/version/version_to_release_test.py
@@ -8,28 +8,31 @@ from valhalla.version.version_to_release import get_release_kinds
 class GetValhallaReleaseKindsTest(unittest.TestCase):
 
     def test_main_file(self):
-        result = get_release_kinds(os.path.dirname(os.path.abspath(__file__)) + "/resources/test_main_file")
+        path = os.path.dirname(os.path.abspath(__file__)) + "/resources/test_main_file"
+        result = get_release_kinds(path)
 
         self.assertEqual(len(result), 1)
 
         self.assertEqual(result[0].filename, "valhalla.yml")
         self.assertEqual(result[0].suffix, "")
-        self.assertEqual(result[0].path, "/home/pz2/repo/valhalla/test/version/resources/test_main_file")
+        self.assertEqual(result[0].path, path)
 
     def test_main_and_hotfix_file(self):
-        result = get_release_kinds(os.path.dirname(os.path.abspath(__file__)) + "/resources/test_main_and_hotfix_file")
+        path = os.path.dirname(os.path.abspath(__file__)) + "/resources/test_main_and_hotfix_file"
+        result = get_release_kinds(path)
 
         self.assertEqual(len(result), 2)
 
         self.assertEqual(result[0].filename, "valhalla.yml")
         self.assertEqual(result[0].suffix, "")
-        self.assertEqual(result[0].path, "/home/pz2/repo/valhalla/test/version/resources/test_main_and_hotfix_file")
+        self.assertEqual(result[0].path, path)
 
         self.assertEqual(result[1].filename, "valhalla-hotfix.yml")
         self.assertEqual(result[1].suffix, "-hotfix")
-        self.assertEqual(result[1].path, "/home/pz2/repo/valhalla/test/version/resources/test_main_and_hotfix_file")
+        self.assertEqual(result[1].path, path)
 
     @patch('valhalla.version.version_to_release.exit')
     def test_empty(self, mock_exit):
-        result = get_release_kinds(os.path.dirname(os.path.abspath(__file__)) + "/resources/empty")
+        path = os.path.dirname(os.path.abspath(__file__)) + "/resources/empty"
+        result = get_release_kinds(path)
         mock_exit.assert_called_with(-1)
