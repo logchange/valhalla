0179fa76cf30752a76c5efcaafe28c8574dcaeef
pz2 <piotr.zmilczak@softnet.com.pl>
2024-06-01 21:36:29 +0200

    Added 3 new predefined variables: VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH

--- valhalla/common/resolver.py
+++ valhalla/common/resolver.py
@@ -4,6 +4,9 @@ import re
 from valhalla.common.logger import info, error
 
 VERSION = "not_set"
+VERSION_MAJOR = "not_set"
+VERSION_MINOR = "not_set"
+VERSION_PATCH = "not_set"
 VERSION_SLUG = "not_set"
 VALHALLA_TOKEN = "not_set"
 CUSTOM_VARIABLES_DICT = dict()
@@ -11,9 +14,15 @@ CUSTOM_VARIABLES_DICT = dict()
 
 def init_str_resolver(version: str, token: str):
     global VERSION
+    global VERSION_MAJOR
+    global VERSION_MINOR
+    global VERSION_PATCH
     global VERSION_SLUG
     global VALHALLA_TOKEN
     VERSION = version
+    VERSION_MAJOR = __get_major(version)
+    VERSION_MINOR = __get_minor(version)
+    VERSION_PATCH = __get_patch(version)
     VERSION_SLUG = __get_slug(version)
     VALHALLA_TOKEN = token
 
@@ -43,10 +52,16 @@ def resolve(string: str):
 
 def __resolve_predefined(string: str):
     global VERSION
+    global VERSION_MAJOR
+    global VERSION_MINOR
+    global VERSION_PATCH
     global VERSION_SLUG
     global VALHALLA_TOKEN
 
     string = string.replace("{VERSION}", VERSION)
+    string = string.replace("{VERSION_MAJOR}", VERSION_MAJOR)
+    string = string.replace("{VERSION_MINOR}", VERSION_MINOR)
+    string = string.replace("{VERSION_PATCH}", VERSION_PATCH)
     string = string.replace("{VERSION_SLUG}", VERSION_SLUG)
     string = string.replace("{VALHALLA_TOKEN}", VALHALLA_TOKEN)
 
@@ -73,3 +88,24 @@ def __get_slug(version):
     slug = re.sub(r'[^0-9a-zA-Z]+', '-', version).lower()
     slug = slug.strip('-')
     return slug
+
+
+def __get_major(version):
+    match = re.match(r'^(\d+)', version)
+    if match:
+        return match.group(1)
+    raise ValueError("Invalid version format")
+
+
+def __get_minor(version):
+    match = re.match(r'^\d+\.(\d+)', version)
+    if match:
+        return match.group(1)
+    raise ValueError("Invalid version format")
+
+
+def __get_patch(version):
+    match = re.match(r'^\d+\.\d+\.(\d+)', version)
+    if match:
+        return match.group(1)
+    raise ValueError("Invalid version format")
