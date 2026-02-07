431159c452c30dfb48a8d71b5fce2e0887d8740b
marwin1991 <peter.zmilczak@gmail.com>
2026-02-08 00:06:06 +0100

    refactor resolver imports in tests for improved clarity and consistency

--- test/common/resolver_test.py
+++ test/common/resolver_test.py
@@ -2,32 +2,31 @@ import os
 import unittest
 from unittest.mock import patch
 
-from valhalla.common.resolver import resolve, init_str_resolver, init_str_resolver_custom_variables, \
-    init_str_resolver_set_version
+import valhalla.common.resolver as resolver
 
 
 class TestStringResolver(unittest.TestCase):
     def test_resolve_no_custom_vars(self):
         # given:
-        init_str_resolver("token123", "kot")
-        init_str_resolver_set_version("1.0")
-        init_str_resolver_custom_variables({})
+        resolver.init_str_resolver("token123", "kot")
+        resolver.init_str_resolver_set_version("1.0")
+        resolver.init_str_resolver_custom_variables({})
 
         # when:
-        resolved_string = resolve("Testing {VERSION}")
+        resolved_string = resolver.resolve("Testing {VERSION}")
 
         # then:
         self.assertEqual("Testing 1.0", resolved_string)
 
     def test_resolve_predefined(self):
         # given:
-        init_str_resolver("token123", "kot")
-        init_str_resolver_set_version("1.0")
-        init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
+        resolver.init_str_resolver("token123", "kot")
+        resolver.init_str_resolver_set_version("1.0")
+        resolver.init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
 
         # when:
-        resolved_string = resolve("Testing {VERSION}")
-        resolved_string_author = resolve("Testing {AUTHOR}")
+        resolved_string = resolver.resolve("Testing {VERSION}")
+        resolved_string_author = resolver.resolve("Testing {AUTHOR}")
 
         # then:
         self.assertEqual("Testing 1.0", resolved_string)
@@ -35,12 +34,12 @@ class TestStringResolver(unittest.TestCase):
 
     def test_resolve_predefined_version(self):
         # given:
-        init_str_resolver("token123", "kot")
-        init_str_resolver_set_version("1.2.14-RC01")
-        init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
+        resolver.init_str_resolver("token123", "kot")
+        resolver.init_str_resolver_set_version("1.2.14-RC01")
+        resolver.init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
 
         # when:
-        resolved_string = resolve(
+        resolved_string = resolver.resolve(
             "Testing {VERSION}; major is {VERSION_MAJOR} and minor is {VERSION_MINOR} and patch {VERSION_PATCH}")
 
         # then:
@@ -48,23 +47,23 @@ class TestStringResolver(unittest.TestCase):
 
     def test_resolve_predefined_version_slug(self):
         # given:
-        init_str_resolver("token123", "kot")
-        init_str_resolver_set_version("1.0.0")
-        init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
+        resolver.init_str_resolver("token123", "kot")
+        resolver.init_str_resolver_set_version("1.0.0")
+        resolver.init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
 
         # when:
-        resolved_string = resolve("some_file_{VERSION_SLUG}.pdf")
+        resolved_string = resolver.resolve("some_file_{VERSION_SLUG}.pdf")
 
         # then:
         self.assertEqual("some_file_1-0-0.pdf", resolved_string)
 
     def test_resolve_custom_variables(self):
         # given:
-        init_str_resolver("token123", "kot")
-        init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
+        resolver.init_str_resolver("token123", "kot")
+        resolver.init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
 
         # when:
-        resolved_string = resolve("Testing {CUSTOM_VAR}")
+        resolved_string = resolver.resolve("Testing {CUSTOM_VAR}")
 
         # then:
         self.assertEqual("Testing value123", resolved_string)
@@ -72,22 +71,21 @@ class TestStringResolver(unittest.TestCase):
     @patch.dict(os.environ, {"ENV_VAR": "env_value123"})
     def test_resolve_from_env(self):
         # given:
-        init_str_resolver("token123", "kot")
-        init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
+        resolver.init_str_resolver("token123", "kot")
+        resolver.init_str_resolver_custom_variables({"CUSTOM_VAR": "value123"})
 
         # when:
-        resolved_string = resolve("Testing {ENV_VAR}")
+        resolved_string = resolver.resolve("Testing {ENV_VAR}")
 
         # then:
         self.assertEqual("Testing env_value123", resolved_string)
 
     def test_resolve_not_initialized_raises_runtime_error(self):
         # given: reset globals (manually because they are persistent in the module)
-        import valhalla.common.resolver as resolver
         resolver.VALHALLA_TOKEN = "not_set"
         
         # when / then:
         with self.assertRaises(RuntimeError) as context:
-            resolve("some string")
+            resolver.resolve("some string")
         
         self.assertIn("There was no init_str_resolver(...) call", str(context.exception))
